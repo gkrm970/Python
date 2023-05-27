205d02f7d78673c06b499b19d5dd004a80f56040
Gopalakrishna R M <108381115+gkrm970@users.noreply.github.com>
2023-05-27 10:06:15 +0530

    Add files via upload

---
+++ 01 Python programming/comments.py
@@ -0,0 +1,17 @@
+# this is python program      ctrl+/
+
+"""this is python program
+this is python program
+this is python program
+this is python program
+this is python program """
+
+
+'''this is python program 
+this is python program 
+this is python program 
+this is python program 
+this is python program '''
+
+print("I'm working with python")
+print('I am working with python')
