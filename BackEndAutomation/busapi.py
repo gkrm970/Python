5c5b1d4328441ed907a84733dd8e41de00c1205a
Gopalakrishna R M <devendra.v@prodapt.com>
2023-08-29 10:37:22 +0530

    Added backend-automation-materials

---
+++ BackEndAutomation/busapi.py
@@ -0,0 +1,27 @@
+import requests
+import json
+response = requests.get('http://10.17.36.156:8088/pubsub/v1/fed_kafka_bus/all_records',)
+# print(response.text)
+# print(type(response.text))
+# dict_response = json.loads(response.text)
+# print(dict_response[0]['isbn'])
+json_response = response.json()
+print(type(json_response))
+print(json_response[0]['isbn'])
+assert response.status_code == 200
+print(response.headers)
+assert response.headers['Content-Type'] == 'application/json;charset=UTF-8'
+
+# Retrieve the book details with ISBN RGHCC
+for actualBook in json_response:
+    if actualBook['isbn'] == 'RGHCC':
+        print(actualBook)
+        break
+
+expectedBook = {
+    "book_name": "Learn API Automation with RestAssured",
+    "isbn": "RGHCC",
+    "aisle": "12239"
+}
+
+assert actualBook == expectedBook
