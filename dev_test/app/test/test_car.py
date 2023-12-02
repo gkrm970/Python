9e9d9cc228250c4e737d8ba8a69b4bc46b4e50f0
Gopalakrishna R M <gopalakrishna.rm@prodapt.com>
2023-12-02 23:18:16 +0530

    Execute mypy correct the code in test_car.py file

--- dev_test/app/test/test_car.py
+++ dev_test/app/test/test_car.py
@@ -6,37 +6,37 @@ from app.app.car import Car
 class TestCar:
 
     @pytest.fixture
-    def my_car(self):
+    def my_car(self) -> Car:
         """Create a new car object"""
         return Car("Ford", "Mustang", 2023)
 
-    def test_accelerate(self, my_car):
+    def test_accelerate(self, my_car: Car) -> None:
         """Test the accelerate method"""
         my_car.accelerate()
         assert my_car.speed == 5
         assert my_car.get_speed() == 5
         assert my_car.get_speed() == my_car.speed
 
-    def test_brake(self, my_car):
+    def test_brake(self, my_car : Car) -> None:
         """Test the brake method"""
         my_car.brake()
         assert my_car.speed == -5
         assert my_car.get_speed() == -5
 
-    def test_get_speed(self, my_car):
+    def test_get_speed(self, my_car: Car) -> None:
         """Test the get_speed method"""
         assert my_car.get_speed() == 0
         assert my_car.speed == 0
 
-    def test_make(self, my_car):
+    def test_make(self, my_car: Car) -> None:
         """Test the make property"""
         assert my_car.make == "Ford"
 
-    def test_model(self, my_car):
+    def test_model(self, my_car: Car) -> None:
         """Test the model property"""
         assert my_car.model == "Mustang"
 
-    def test_year(self, my_car):
+    def test_year(self, my_car: Car) -> None:
         """Test the year property"""
         assert my_car.year == 2023
 
