c33c0e710988bda1659f2b7aee94d8576ecd7eda
Gopalakrishna R M <gopalakrishna.rm@prodapt.com>
2023-12-03 00:19:10 +0530

    refactor the whole code

--- dev_test/app/app/car.py
+++ dev_test/app/app/car.py
@@ -1,17 +1,26 @@
 class Car:
-    def __init__(self, make, model, year):
+
+    # Accelerate rate
+    accelerate_rate = 5
+
+    # Brake rate
+    brake_rate = 5
+
+    def __init__(self, make: str, model: str, year: int) -> None:
         self.make = make
         self.model = model
         self.year = year
         self.speed = 0
 
-    def accelerate(self):
-        self.speed += 5
+    def accelerate(self) -> int | None:
+        self.speed += self.accelerate_rate
+        return self.speed
 
-    def brake(self):
-        self.speed -= 5
+    def brake(self) -> int | float | None:
+        self.speed -= self.brake_rate
+        return self.speed
 
-    def get_speed(self):
+    def get_speed(self) -> int | float | None:
         return self.speed
 
 
--- dev_test/app/test/test_car.py
+++ dev_test/app/test/test_car.py
@@ -10,23 +10,28 @@ class TestCar:
         """Create a new car object"""
         return Car("Ford", "Mustang", 2023)
 
+    year = 2023
+    accelerate_rate = 5
+    brake_rate = -5
+    speed_after_accelerate = 5
+    current_speed = 0
+
     def test_accelerate(self, my_car: Car) -> None:
         """Test the accelerate method"""
         my_car.accelerate()
-        assert my_car.speed == 5
-        assert my_car.get_speed() == 5
-        assert my_car.get_speed() == my_car.speed
+        assert my_car.speed == self.speed_after_accelerate
+        assert my_car.get_speed() == self.speed_after_accelerate
 
-    def test_brake(self, my_car : Car) -> None:
+    def test_brake(self, my_car: Car) -> None:
         """Test the brake method"""
         my_car.brake()
-        assert my_car.speed == -5
-        assert my_car.get_speed() == -5
+        assert my_car.speed == self.brake_rate
+        assert my_car.get_speed() == self.brake_rate
 
     def test_get_speed(self, my_car: Car) -> None:
         """Test the get_speed method"""
-        assert my_car.get_speed() == 0
-        assert my_car.speed == 0
+        assert my_car.get_speed() == self.current_speed
+        assert my_car.speed == self.current_speed
 
     def test_make(self, my_car: Car) -> None:
         """Test the make property"""
@@ -38,6 +43,7 @@ class TestCar:
 
     def test_year(self, my_car: Car) -> None:
         """Test the year property"""
-        assert my_car.year == 2023
+        assert my_car.year == self.year
+
 
 # Run the tests
