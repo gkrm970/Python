9ca351fc34014f847cedd80dc24814d662f90886
Gopalakrishna R M <gopalakrishna.rm@prodapt.com>
2023-12-03 21:42:23 +0530

    Add exception concept and it codes add file handling code

--- language/oops/exception_handling/Exceptions.py
+++ language/oops/exception_handling/Exceptions.py
@@ -1,58 +1,80 @@
 # Example1
-# print("this is starting point of program...")
-# print("this is starting point of program...")
-# print("this is starting point of program...")
+# print("this is a starting point of program...")
+# print("this is a starting point of program...")
+# print("this is a starting point of program...")
 
 # try:
 #     print("xyz")
 # except:
 #     print("Exception occurred")
 
-# print("this is end of the program..")
+# print("this is end of the program.")
 # print("this is end of the program..")
 # print("this is end of the program..")
 
 # Example2
-print("This is starting point of program.....")
-print("Program in progress")
-try:
-    print(5/0)   #2.0
-except:
-    print("Exception occurred and handled..")
+# print("This is starting point of program.....")
+# print("Program in progress")
+
+# try:
+#     print(5 / 0)
+# except:
+#     print("Exception occurred and handled.")
 
 # print("Program completed...")
 
 # Example3 : Multiple except blocks  - try, except else, finally
 # try:
-#     num1,num2=10,2
-#     result=num1/num2
-#     print("result is:",result)
+#     num1, num2 = 10, 2
+#     result = num1 / num2
+#     print("result is:", result)
 # except ZeroDivisionError:
 #     print("Thrown ZeroDivisionError exception....")
 # except SyntaxError:
 #     print("Thrown Syntax error exception...")
-# except:
-#     print("Exception handled...")
+# except Exception as e:
+#     print("Exception handled... ", e)
 # else:
 #     print("No exceptions occurred...")
 # finally:
 #     print("always execute...")
 
-# Example4 : raising our own exceptions
-# def enterage(num):
-#     if num<0:
-#         raise ValueError("Only Integers are allowed")
-#     if num%2==0:
-#         print("even")
-#     else:
-#         print("odd")
-# # enterage(-1)
+# Example4: raising our own exceptions
+def enter_age(number):
+    if number < 0:
+        raise ValueError("Only Integers are allowed")
+    if number % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+
+enter_age(-1)
 #
-# print("checking number is even or odd by calling function..")
-# num=0
-# try:
-#     enterage(num)
-# except ValueError:
-#     print("value error exception occurred and handled..")
-# print("program completed....")
-#
+print("checking number is even or odd by calling function.")
+number = 0
+try:
+    enter_age(number)
+except ValueError:
+    print("value error exception occurred and handled..")
+
+except TypeError:
+    print("Type error exception occurred and handled..")
+
+except ZeroDivisionError:
+    print("ZeroDivisionError exception occurred and handled..")
+
+except SyntaxError:
+    print("SyntaxError exception occurred and handled..")
+
+except NameError:
+    print("NameError exception occurred and handled..")
+
+except AttributeError:
+    print("AttributeError exception occurred and handled..")
+
+except Exception as e:
+    print("Exception occurred and handled..", e)
+finally:
+    print("program completed....")
+
--- language/oops/exception_handling/HandlingFiles.py
+++ language/oops/exception_handling/HandlingFiles.py
@@ -1,4 +1,4 @@
-#Example1: writing data in to text file
+# Example1: writing data in to text file
 # file=open("C:\Demofiles\myfile.txt",'w')
 #
 # file.write("This is my first statement...\n")
@@ -9,22 +9,75 @@
 # file.close()
 # print("program completed....")
 
-#Example2: reading data from text file
+# Example2: reading data from text file
 
 # file=open("C:\Demofiles\myfile.txt",'r')
 # #print(file.read())
 # print(file.readline())
 # file.close()
 
-#Example3 : appending data into text file
+# Example3: appending data into text file
 # file=open("C:\Demofiles\myfile.txt",'a')
 #
 # file.write("\n this is my sixth line\n")
 # file.write(" this is my seventh line\n")
 # file.close()
 # print("program is completed....")
-#
 
+# Example4: reading data from text file using with block
+# with open("C:\Demofiles\myfile.txt",'r') as file:
+#     print(file.read())
+#     print(file.readline())
+# print("program completed....")
 
+# Example5: writing data into text file using with block
+
+# with open("C:\Demofiles\myfile.txt", 'w') as file:
+#     file.write("This is my first statement...\n")
+#     file.write("This is my second statement...\n")
+#     file.write("This is my third statement...\n")
+#     file.write("This is my fourth statement...\n")
+#     file.write("This is my fith statement...")
+    # file.close() is not required when we use with block
+
+print("program completed....")
+
+# Example6: appending data into text file using with block
+# with open("C:\Demofiles\myfile.txt",'a') as file:
+#     file.write("\n this is my sixth line\n")
+#     file.write(" this is my seventh line\n")
+# print("program is completed....")
+
+# Example7: reading data from text file using for loop
+# with open("C:\Demofiles\myfile.txt",'r') as file:
+#     for line in file:
+#         print(line)
+# print("program completed....")
+
+# Example8: writing data into text file using for loop
+# with open("C:\Demofiles\myfile.txt", 'w') as file:
+#     for i in range(1, 10):
+#         file.write("This is my first statement...\n")
+# print("program completed....")
+
+# Example9: appending data into text file using for loop
+# with open("C:\Demofiles\myfile.txt",'a') as file:
+#     for i in range(1, 10):
+#         file.write(" \n This is my first statement...\n")
+# print("program completed....")
+
+# Example10: reading data from text file using readline()
+# with open("C:\Demofiles\myfile.txt",'r') as file:
+#     print(file.readline())
+# print("program completed....")
+
+# Example11: writing data into text file using readline()
+# with open("C:\Demofiles\myfile.txt", 'w') as file:
+#     file.write("This is my first statement...\n")
+#     file.write("This is my second statement...\n")
+#     file.write("This is my third statement...\n")
+#     file.write("This is my fourth statement...\n")
+#     file.write("This is my fith statement...")
+# print("program completed....")
 
 
