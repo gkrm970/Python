2eb34bc34b38ab85a8e3e569557e0d570ddf9c08
Gopalakrishna R M <gopalakrishna.rm@prodapt.com>
2024-03-19 10:48:44 +0530

    Add python modules quiz materials

---
+++ language/PCEP/modules/operator.py
@@ -0,0 +1,154 @@
+"""
+What is the output of print(2 * 3 ** 3 * 4)?
+216
+
+What is the value of the expression 100 / 50 ?
+2.0
+
+What is the output of the following python code?
+
+print(2*(2+3))
+10
+
+Fill in the missing operators:
+
+20 ... 5 ... 4 = 16.0
+
+/ and * -correct
+/ and //
+* and //
+
+What is the output of the following python code?
+
+print(10 - 6 ** 2 / 9 * 10 + 1)
+ -29.0
+
+What is the output of the following python code?
+
+print(2 ** 3)
+8
+
+What is the output of the following python code?
+
+print(2 ** 3.)
+8.0
+
+What is the output of the following python code?
+
+print(13 / 4 + 13 % 4)
+
+4.25
+
+What does the following print?
+
+x = 10 / 4
+y = 5 / 2.0
+print (x + y)
+
+What is the output of the following python code?
+
+print(10 / 2)
+5.0
+
+What is the output of the following python code?
+
+print(9 % 4)
+
+What is the output of the following python code?
+
+print(6. // 4)
+
+
+"""
+
+# print(2 * 3 ** 3 * 4)
+# print(100 / 50)
+# print(4 / 2)
+# print(2 * (2 + 3))
+#
+# print(10 - 6 ** 2 / 9 * 10 + 1)
+# print(2 ** 3)
+#
+# print(2 ** 3.0)
+#
+# print(13 / 4 + 13 % 4)
+#
+# x = 10 / 4
+# y = 5 / 2.0
+# print (x + y)
+# print(9 % 4)
+# print(6. // 4)
+
+
+# End module
+"""
+What is the output of the following python code?
+
+x = 6
+y = 7
+print(x == y)
+
+True
+
+What is the output of the following python code?
+
+x = 6
+print(x > 4 and x < 12)
+
+True
+
+Which of the following will evaluate to true?
+
+False AND (False OR True) = False
+False AND (True OR True) = false
+False or True = True  ---->when condition exist it is always true
+True AND false = False
+
+What will be the output of the following Python code?
+
+is_hungry = True
+if(not is_hungry):
+  print("You are not hungry")
+else:
+  print("You are hungry")
+
+You are hungry
+
+What will be the output of the following Python code?
+
+is_hungry = False
+if(not is_hungry):
+  print("You are not hungry")
+else:
+  print("You are hungry")
+
+You are not hungry
+
+Which python operator means 'less than or equal to'?
+
+<=
+
+What is the output of the following python code?
+
+x = 6
+y = 7
+print(x == y)
+
+false
+
+Which python operator means 'bigger than or equal to'?
+
+>=
+
+
+
+
+"""
+# x = 6
+# print(x > 7 or x < 12)
+#
+# is_hungry = True
+# if not is_hungry:
+#     print("You are not hungry")
+# else:
+#     print("You are hungry")
