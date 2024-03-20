614eb714f9e044c696703132549e3f837100b33c
Gopalakrishna R M <gopalakrishna.rm@prodapt.com>
2024-03-20 17:16:48 +0530

    Add exam1 material for PCEP

---
+++ language/PCEP/exam1.py
@@ -0,0 +1,153 @@
+"""
+Given the nested if-else below, what will be the value x when the code executed successfully.
+
+x = 0
+a = 6
+b = 6
+if a > 0:
+    if b < 0:
+        x = x + 6
+    elif a > 6:
+        x = x + 5
+    else:
+        x = x + 4
+else:
+    x = x + 3
+
+print(x)
+
+4
+
+What will be the output of below Python code?
+
+list1 = [10, 11, 12, 13, 14]
+print(list1[0])
+
+10
+
+What will be the output of below Python code?
+
+countries = ["USA", "Canada", "India"]
+countries[0], countries[1] = countries[1], countries[0]
+print(countries)
+
+['Canada', 'USA', 'India']
+
+What will be the output of the below Python code?
+
+list1=['UK','India','Canada']
+
+list1.insert(1,8)
+
+print(list1)
+
+['UK', 8, 'India', 'Canada']
+
+What will be the result of calling the fullname_func function with the arguments 'John' and 'Mark'?
+
+def fullname_func(fname, lname):
+  print(fname + " " + lname)
+
+fullname_func("John", "Mark")
+
+John Mark
+
+…. is one of the literal types in Python.
+
+string
+int
+float
+bool
+list
+tuple
+dict
+set
+
+The function can have only one parameter. If any data (parameters) are passed, they are passed explicitly.
+
+false
+
+The …. function prints the specified message to the screen, or another standard output device.
+print()
+
+Which method should you use in order to convert the input into a string correctly:
+
+year_of_birth = int(input("In what year were you born? "))
+
+print("You were born in " + ...(year_of_birth))
+
+str
+
+What will be the output of below Python code?
+
+list1 = [1, 2, 3, 4, 5]
+for i in list1:
+    if i==5:
+        print(i)
+5
+
+Which of the following method is used to delete a brand key and its value from the following dictionary:
+
+testdict = {'brand': 'oppo', 'ram': '3', 'Os': 'Android', 'year': 2020}
+
+del testdict['brand']
+
+Which of the following statements assigns the value 50 to the variable x in Python:
+What is the output of the following nested loop:
+
+for num in range(5, 9):
+   for i in range(2, num):
+       if num%i == 1:
+          print(num)
+          break
+
+5
+6
+7
+8
+
+What will be the output of the following Python code?
+
+matrix = [[j for j in range(3)] for i in range(3)]
+print(matrix[1][2])
+
+2
+
+What does the following code produce as output?
+
+i = 1
+x = 3
+sum = 0
+while ( i <= x ):
+ sum += i
+ i += 1
+print(sum)
+
+…. is a string method used to convert string into a lower case.
+
+lower()
+casefolder()
+
+
+
+
+
+
+
+
+"""
+# for num in range(5, 9):
+#     for i in range(2, num):
+#         if num % i == 1:
+#             print(num)
+#             break
+
+# i = 1
+# x = 3
+# sum = 0
+# while i <= x:
+#     sum += i
+#     i += 1
+# print(sum)
+
+# print(2 + 2)
