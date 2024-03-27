3d2147022f9f9537a5055bb53541c15b33ffe76e
Gopalakrishna R M <gopalakrishna.rm@prodapt.com>
2024-03-27 10:56:11 +0530

    Added multiple browser id optional

--- automation_with_selenium/test_cases/conftest.py
+++ automation_with_selenium/test_cases/conftest.py
@@ -4,6 +4,23 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture()
-def setup():
-    driver = webdriver.Chrome()
+def setup(browser):
+    if browser == "firefox":
+        driver = webdriver.Firefox()
+
+    elif browser == "edge":
+        driver = webdriver.Edge()
+    else:
+        driver = webdriver.Chrome()
     return driver
+
+
+# this will get the value from CLI/hooks - this is a special type of method
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+
+# this will return the browser value to setup method
+@pytest.fixture()
+def browser(request):
+    return request.config.getoption("--browser")
